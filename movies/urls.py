eadf147868c15f003824ecf4d12eadb0e8eeba78
lingsong <lingsong@lingsongs-MBP.wireless.yale.internal>
2018-12-11 04:03:53 -0500

    small change

--- movies/urls.py
+++ movies/urls.py
@@ -6,6 +6,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^show_page/$', views.show_all, name='show_all'),
+    url(r'^movies/$', views.movies, name='movies'),
     url(r'^search/$', views.search, name='search'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -24,7 +24,7 @@ class MovieViewSet(viewsets.ModelViewSet):
 	queryset = Movie.objects.all()
 	serializer_class = MovieSerializer
 
-def show_all(request):
+def movies(request):
     movies = Movie.objects.all()
     limit = 10
     paginator = Paginator(movies, limit) # 10 records per page

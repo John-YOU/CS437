4df407050e5bd0bb7a72f2302668bf45ea5bacf8
lingsong <lingsong@lingsongs-MBP.wireless.yale.internal>
2018-12-11 00:34:26 -0500

    database models

--- movies/models.py
+++ movies/models.py
@@ -41,13 +41,13 @@ class AuthUser(models.Model):
     password = models.CharField(max_length=128)
     last_login = models.DateTimeField(blank=True, null=True)
     is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150)
     first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     email = models.CharField(max_length=254)
     is_staff = models.BooleanField()
     is_active = models.BooleanField()
     date_joined = models.DateTimeField()
-    last_name = models.CharField(max_length=150)
+    username = models.CharField(unique=True, max_length=150)
 
     class Meta:
         managed = False
@@ -76,15 +76,59 @@ class AuthUserUserPermissions(models.Model):
         unique_together = (('user', 'permission'),)
 
 
+class CastPeople(models.Model):
+    person_id = models.TextField(db_column='Person_ID', blank=True, null=True)  # Field name made lowercase.
+    name = models.TextField(db_column='Name', blank=True, null=True)  # Field name made lowercase.
+    birth_year = models.TextField(db_column='Birth Year', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    death_year = models.TextField(db_column='Death Year', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+
+    class Meta:
+        managed = False
+        db_table = 'cast_people'
+
+
+class CastRe(models.Model):
+    movie_id = models.TextField(db_column='Movie_ID', blank=True, null=True)  # Field name made lowercase.
+    person_id = models.TextField(db_column='Person_ID', blank=True, null=True)  # Field name made lowercase.
+    characters = models.TextField(blank=True, null=True)
+    category = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'cast_re'
+
+
+class CrewPeople(models.Model):
+    person_id = models.TextField(db_column='Person_ID', blank=True, null=True)  # Field name made lowercase.
+    name = models.TextField(db_column='Name', blank=True, null=True)  # Field name made lowercase.
+    birth_year = models.TextField(db_column='Birth Year', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    death_year = models.TextField(db_column='Death Year', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    primary_profession = models.TextField(db_column='Primary Profession', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+
+    class Meta:
+        managed = False
+        db_table = 'crew_people'
+
+
+class CrewRe(models.Model):
+    movie_id = models.TextField(db_column='Movie_ID', blank=True, null=True)  # Field name made lowercase.
+    person_id = models.TextField(db_column='Person_ID', blank=True, null=True)  # Field name made lowercase.
+    category = models.TextField(db_column='Category', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'crew_re'
+
+
 class DjangoAdminLog(models.Model):
     id = models.IntegerField(primary_key=True)  # AutoField?
-    action_time = models.DateTimeField()
     object_id = models.TextField(blank=True, null=True)
     object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
     change_message = models.TextField()
     content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    action_flag = models.PositiveSmallIntegerField()
+    action_time = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -123,6 +167,39 @@ class DjangoSession(models.Model):
         db_table = 'django_session'
 
 
+class GenreRe(models.Model):
+    movie_id = models.TextField(db_column='Movie_ID', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    genre_id1 = models.TextField(db_column='genre_ID1', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    genre_id2 = models.TextField(db_column='genre_ID2', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    genre_id3 = models.TextField(db_column='genre_ID3', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'genre_re'
+
+
+class GenresTable(models.Model):
+    genres_name = models.TextField(db_column='Genres_Name', blank=True, null=True)  # Field name made lowercase.
+    genres_id = models.TextField(db_column='Genres_ID', blank=True, null=True)  # Field name made lowercase.
+    description = models.TextField(db_column='Description', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'genres_table'
+
+
+class Movie(models.Model):
+    movie_id = models.TextField(db_column='Movie_ID', blank=True, primary_key=True)  # Field name made lowercase.
+    primary_title = models.TextField(db_column='Primary Title', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    isadult = models.IntegerField(db_column='isAdult', blank=True, null=True)  # Field name made lowercase.
+    year = models.IntegerField(db_column='Year', blank=True, null=True)  # Field name made lowercase.
+    runtime = models.IntegerField(db_column='Runtime', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'movie'
+
+
 class MoviesGenre(models.Model):
     id = models.IntegerField(primary_key=True)  # AutoField?
     name = models.CharField(max_length=50)
@@ -143,3 +220,29 @@ class MoviesMovie(models.Model):
     class Meta:
         managed = False
         db_table = 'movies_movie'
+
+
+class Rating(models.Model):
+    x = models.IntegerField(db_column='X', blank=True, null=True)  # Field name made lowercase.
+    movie_id = models.TextField(db_column='Movie_ID', blank=True, null=True)  # Field name made lowercase.
+    average_rating = models.FloatField(db_column='Average.Rating', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    number_of_votes = models.IntegerField(db_column='Number.of.Votes', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    level = models.TextField(db_column='Level', blank=True, null=True)  # Field name made lowercase.
+    popular = models.TextField(db_column='Popular', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'rating'
+
+
+class Title(models.Model):
+    movie_id = models.TextField(db_column='Movie_ID', blank=True, null=True)  # Field name made lowercase.
+    version_order = models.IntegerField(db_column='Version_order', blank=True, null=True)  # Field name made lowercase.
+    title = models.TextField(db_column='Title', blank=True, null=True)  # Field name made lowercase.
+    region = models.TextField(db_column='Region', blank=True, null=True)  # Field name made lowercase.
+    language = models.TextField(db_column='Language', blank=True, null=True)  # Field name made lowercase.
+    isoriginaltitle = models.IntegerField(db_column='isOriginalTitle', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'title'
